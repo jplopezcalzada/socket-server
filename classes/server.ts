d8371541ea7b06d623477aa584bf4c08559c22a0
jplopezcalzada <jplopezcalzada@gmail.com>
2020-09-24 00:06:57 +0200

    Primer commit de rest server

--- classes/server.ts
+++ classes/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { SERVER_PORT } from '../global/enviroment';
+import { SERVER_PORT } from '../global/environment';
 
 export default class Server {
     public app: express.Application;
@@ -13,9 +13,10 @@ export default class Server {
         this.port = SERVER_PORT;
     }
 
-    start( callback: Function) {
+    start( callback: Function ) {
 
-        this.app.listen( this.port,callback);
+        this.app.listen( this.port, callback );
+
     }
 
 
